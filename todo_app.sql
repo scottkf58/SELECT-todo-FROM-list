da8c561e2b6376fa096c27a207d451a56e0faa7f
Scott Fujimoto <scottfujimoto@Scotts-MacBook-Pro.local>
2017-07-22 15:43:47 -1000

    deleting mistake 1

--- todo_app.sql
+++ todo_app.sql
@@ -73,6 +73,20 @@ SELECT title
 FROM tasks
 WHERE title LIKE '%mistake%';
 
+DELETE FROM tasks
+WHERE title = 'mistake 1';
+
+SELECT title, description
+FROM tasks
+WHERE title LIKE '%mistake%';
+
+DELETE FROM tasks
+WHERE title LIKE '%mistake%';
+
+SELECT * FROM tasks ORDER BY title ASC;
+
+
+
 
 
 
